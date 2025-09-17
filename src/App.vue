d14e5a4c09617a675714232366516974b1bc866b
Marie-Ève Noël <marie-evenoel@Marie-Eves-MacBook-Pro.local>
2025-09-17 13:15:30 +0200

    change colors for light scheme

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Cafe Filo logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Bienvenido al cafe Filo!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+       
       </nav>
     </div>
   </header>
@@ -20,7 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,11 +6,10 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="dark-red">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Quieres saber mas sobre nosotros?
+     
     </h3>
   </div>
 </template>
