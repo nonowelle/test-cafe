47f9d1b611834c11c31d957fa3f9f65bdf72a04e
Marie-Ève Noël <menoel888@gmail.com>
2025-10-22 22:03:27 +0200

    remove dev tool

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    process.env.NODE_ENV === 'development' && vueDevTools(),
+  ].filter(Boolean),
 
   server: {
     proxy: {
       '/form': 'http://localhost:3000'
     },
   },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
